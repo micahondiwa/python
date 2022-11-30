c458523dd9a8eadf2faee76f3f52f427d8789ea0
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-11-30 12:29:55 +0300

    Random

---
+++ 0x00-python/9-random.py
@@ -0,0 +1,14 @@
+'''
+Printing random values using the while loop
+'''
+
+import random
+
+random_num = random.randrange(1,51)
+
+i = 11
+while (i != random_num):
+    i += 1
+
+print("The number value is: ", random_num)
+
