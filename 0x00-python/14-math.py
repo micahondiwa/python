6168b9e9fb47404ffaa1523f24a07729e28b6fa1
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-11-30 16:39:43 +0300

    math

---
+++ 0x00-python/14-math.py
@@ -0,0 +1,15 @@
+'''
+Basic mathemtical operations using the math library
+'''
+import math
+
+print("ceil(4.4) = ", math.ceil(4.4))
+print("floor(4.4 = ", math.floor(4.4))
+print("fabs(-4.4 = ", math.fabs(-4.4))
+print("factorial(4) = ", math.factorial(4))
+print("fmod(5,4 = ", math.fmod(5,4))
+print("trunc(4.2 =", math.trunc(4.2))
+print("pow(2,2) = ", math.pow(2,2))
+
+
+
