88631f4150b8c92af1eac65fea3234a3366a76dc
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-11-30 09:33:09 +0300

    Hello user

--- 0x00-python/0-hello.py
+++ 0x00-python/0-hello.py
@@ -1,5 +1,5 @@
 '''
-sk user to enter their name  and store it a variable called name
+Ask user to enter their name  and store it a variable called name
 Print out "Hello, name"
 '''
 name  = input("What is your name?")
