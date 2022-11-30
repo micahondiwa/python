e3d430d07f16dc81066fca4f15e86262debe8cda
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-11-30 11:45:12 +0300

    Odds

---
+++ 0x00-python/6-print_odds.py
@@ -0,0 +1,9 @@
+'''
+* Use for loop through the list from 1 to 21
+* Use modulus to check that the result is NOT Equal to 0
+* Print the odds
+'''
+
+for i in range(1 , 21):
+    if (i % 2 != 0):
+        print(i)
