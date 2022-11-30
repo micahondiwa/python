7965114f64a57ed925390ced07a103dcc0892e5a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-11-30 16:46:15 +0300

    Math

--- 0x00-python/14-math.py
+++ 0x00-python/14-math.py
@@ -2,6 +2,8 @@
 Basic mathemtical operations using the math library
 '''
 import math
+from decimal import Decimal as D
+
 
 print("ceil(4.4) = ", math.ceil(4.4))
 print("floor(4.4 = ", math.floor(4.4))
@@ -11,5 +13,13 @@ print("fmod(5,4 = ", math.fmod(5,4))
 print("trunc(4.2 =", math.trunc(4.2))
 print("pow(2,2) = ", math.pow(2,2))
 
+sum = D(0)
+sum += D("0.1")
+sum += D("0.1")
+sum += D("0.1")
+sum -= D("0.3")
 
+print("sum = ", sum)
+
+print(".1 + .1 + .1 -.3 = ", .1 + .1 + .1 -.3)
 
