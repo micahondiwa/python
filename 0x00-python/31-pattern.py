b4e96ad4faf8424aaa8f5160cbd62e7f900ecca7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-06 16:42:04 +0300

    Patter using for loop

---
+++ 0x00-python/31-pattern.py
@@ -0,0 +1,9 @@
+for i in range (1):
+  for j in range (1):
+    print(0, 0, "X")
+for i in range (1):
+  for j in range (1):
+    print(0, "X", 0)
+for i in range (1):
+  for j in range (1):
+    print("X", 0, 0)
