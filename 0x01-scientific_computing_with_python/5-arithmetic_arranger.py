d3c5d59ea3fa3904c2e1717caf795a70c34bc391
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-01 10:16:26 +0300

    Arranger

--- 0x01-scientific_computing_with_python/5-arithmetic_arranger.py
+++ 0x01-scientific_computing_with_python/5-arithmetic_arranger.py
@@ -33,4 +33,6 @@ def arithmetic_arranger(problems, show_answers=False):
         
         # Width for formatting
         width = max([len(problem[0]), problem[1]]) + 2
+
+        # Calculating answers if show_answers = True
         
