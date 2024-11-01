b9d33b51d9cabd48e0d9c90ddce773565e04090c
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-01 18:20:55 +0300

    Password generator"

--- 0x01-scientific_computing_with_python/6-password_generator.py
+++ 0x01-scientific_computing_with_python/6-password_generator.py
@@ -19,8 +19,30 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
             password += secrets.choice(all_characters)
         
         constraints = [(nums, '')]
-    
-    return password
+        import re
+import secrets
+import string
+
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, '')
+        ]
+        pattern = re.compile('i')        
 
 new_password = generate_password(8)
 print(new_password)
