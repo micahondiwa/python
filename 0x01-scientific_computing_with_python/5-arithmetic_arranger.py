d2bc137fa4125689ed3c12f8113afad534a5dc76
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-01 09:00:44 +0300

    Arranger

--- 0x01-scientific_computing_with_python/5-arithmetic_arranger.py
+++ 0x01-scientific_computing_with_python/5-arithmetic_arranger.py
@@ -2,15 +2,6 @@ problem = ["3801 - 2", "123 + 49"]
 
 # print("3801      123\n-    2    +  49\n------    -----.")
 
-def extract_elements(lists):
-    result = []
-    for item in lists:
-        part1, sep, part2 = item.partition('+' if '+' in item else '-')
-        result.append([part1, sep, part2])
-    return result
-#problem1 = print(extract_elements(problem))
-
-
 def extracts(problem):
     return problem.partition(('+' if '+' in problem else '-') for problem in problem)
 
