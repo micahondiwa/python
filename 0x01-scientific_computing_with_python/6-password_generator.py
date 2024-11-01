a58f901d53ed34ea3dc9f8719446626fd4680fe0
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-01 18:37:50 +0300

    Password generator"

--- 0x01-scientific_computing_with_python/6-password_generator.py
+++ 0x01-scientific_computing_with_python/6-password_generator.py
@@ -40,7 +40,8 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
             password += secrets.choice(all_characters)
         
         constraints = [
-            (nums, '[0-9]')
+            (nums, '[0-9]'),
+            (lowercase, 'a-z')
         ]
         pattern = ('[^a-z]t') 
         quote = 'Not all those who wander are lost.'       
