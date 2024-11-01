763349400f66b4cf7adb1da969de82660b88d2b3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-01 10:30:59 +0300

    Arranger

--- 0x01-scientific_computing_with_python/5-arithmetic_arranger.py
+++ 0x01-scientific_computing_with_python/5-arithmetic_arranger.py
@@ -58,4 +58,6 @@ def arithmetic_arranger(problems, show_answers=False):
         arranged += "\n" + answers + "\n" + underline
     
     return arranged
-    
+
+# Example implentation
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
