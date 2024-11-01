da6a48c002974cd71bad046ca894ced858a30d4f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-01 19:40:46 +0300

    Arranger

--- 0x01-scientific_computing_with_python/6-password_generator.py
+++ 0x01-scientific_computing_with_python/6-password_generator.py
@@ -33,5 +33,6 @@ def generate_password(length=16,nums = 1,special_chars=1,uppercase=1, lowercase=
     
     return password
 
-new_password = generate_password()
-print("Generated password: ",new_password)
+if __name__ == '__main__':
+    new_password = generate_password()
+    print("Generated password: ",new_password)
