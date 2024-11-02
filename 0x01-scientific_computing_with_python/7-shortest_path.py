1ca633dd9f3b25003d47354beb6fc27077b13389
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-02 09:00:29 +0300

    Shortest path

--- 0x01-scientific_computing_with_python/7-shortest_path.py
+++ 0x01-scientific_computing_with_python/7-shortest_path.py
@@ -2,10 +2,10 @@ copper = {
     'species': 'guinea pig',
     'age': 2
     }
-copper['food'] = 'hay'
-del copper['age']
+# copper['food'] = 'hay'
+# del copper['age']
 # for i in copper.values():
 #     print(i)
 
-for i, j in copper.items():
-    print(f'{i}: {j}')
+# for i, j in copper.items():
+#     print(f'{i}: {j}')
