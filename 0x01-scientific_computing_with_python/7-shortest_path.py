3917b6d890e24b5c9e7815a5cc6668432d6b9625
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-03 15:43:13 +0300

    shortest path

--- 0x01-scientific_computing_with_python/7-shortest_path.py
+++ 0x01-scientific_computing_with_python/7-shortest_path.py
@@ -18,7 +18,7 @@ def shortest_path(graph, start):
             if distance + distances[current] < distances[node]:
                 distances[node] = distance + distances[current]
                 if paths[node] and paths[node] == node:
-                    paths[node] == paths[current]
+                    paths[node] == paths[current][:]
                 else:
                     paths[node].extend(paths[current])
                 paths[node].append(node)
