0ccca89c3396f59b0837b57cfcb326e994e8c540
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-03 08:51:49 +0300

    Shortest path

--- 0x01-scientific_computing_with_python/6-password_generator.py
+++ 0x01-scientific_computing_with_python/6-password_generator.py
@@ -36,3 +36,4 @@ def generate_password(length=16,nums = 1,special_chars=1,uppercase=1, lowercase=
 if __name__ == '__main__':
     new_password = generate_password()
     print("Generated password: ",new_password)
+    
