a88705ff32534e97da6479f84cf0ad51fde93276
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-04 07:01:35 +0300

    Tower of Hanoi puzzle

--- 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
+++ 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
@@ -13,7 +13,7 @@ def move(n, source, auxiliary, target):
     # display starting configuration
     print(rods)
     for i in range(number_of_moves):
-        if (i + 1) % 3 == 1:
+        if remainder == 1:
             print(f'Move {i + 1} allowed between {source} and {target}')
 
 # initiate call from source A to target C with auxiliary B
