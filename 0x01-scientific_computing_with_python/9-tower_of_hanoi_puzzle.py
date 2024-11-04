be4fc214fd690853c05ff71a39ebca805c8fe6a5
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-04 07:04:48 +0300

    Tower of Hanoi puzzle

--- 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
+++ 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
@@ -16,6 +16,8 @@ def move(n, source, auxiliary, target):
         remainder = (i + 1) % 3
         if remainder == 1:
             print(f'Move {i + 1} allowed between {source} and {target}')
+        elif remainder == 2:
+            
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
