f1d99ea14a4858facb68b8ca98d4f9035ec07c1c
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-04 05:16:21 +0300

    shortest path

--- 0x01-scientific_computing_with_python/7-shortest_path.py
+++ 0x01-scientific_computing_with_python/7-shortest_path.py
@@ -1,4 +1,6 @@
 
+'''7-shortest_path.py'''
+
 def shortest_path(graph, start, target=''):
     unvisited = list(graph)
     distances = {node: 0 if node == start else float('inf') for node in graph}
