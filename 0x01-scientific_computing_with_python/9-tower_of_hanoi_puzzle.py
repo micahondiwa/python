ab1253d61ccc8e6240d1a43cf8a2956d9569b8d0
micahondiwa <micah.oketch.ondiwa@gmail.ccom>
2024-11-12 10:26:19 +0300

    Pop the last element from the source and append it to the target

--- 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
+++ 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
@@ -23,7 +23,7 @@ def move(n, source, auxiliary, target):
                 rods[target].append(rods[source].pop())
             else:
                 print(f'Moving disk {rods[target][-1]} from {target} to {source}')
-                rods[source],append(rods[target].pop())
+                rods[source].append(rods[target].pop())
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
         elif remainder == 0:
