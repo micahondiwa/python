3192b7485645e2bd58a09ea899332e498caa4b00
micahondiwa <micah.oketch.ondiwa@gmail.ccom>
2024-11-12 14:55:48 +0300

    make a move

--- 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
+++ 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
@@ -5,36 +5,33 @@ rods = {
     'B': [],
     'C': []
 }
-
-def make_alloed_move(rod1, rod2):
-    pass 
-
+def make_allowed_move(rod1, rod2):
+    forward = False
+    if not rods[target]:
+        forward = True
+    elif rods[source] and rods[source][-1] < rods[target][-1]:
+        forward = True
+    if forward:
+        print(f'Moving disk {rods[source][-1]} from {source} to {target}')
+        rods[target].append(rods[source].pop())
+    else:
+        print(f'Moving disk {rods[target][-1]} from {target} to {source}')
+        rods[source].append(rods[target].pop())
+        
+    # display our progress
+    print(rods)
 
 def move(n, source, auxiliary, target):
-    # displaying starting configuration
+    # display starting configuration
     print(rods)
     for i in range(number_of_moves):
         remainder = (i + 1) % 3
         if remainder == 1:
             print(f'Move {i + 1} allowed between {source} and {target}')
-            forward = False
-            if not rods[target]:
-                forward = True
-            elif rods[source] and rods[source][-1] < rods[target][-1]:
-                forward = True
-            if forward == True:
-                print(f'Moving disk {rods[source][-1]} from {source} to {target}')
-                rods[target].append(rods[source].pop())
-            else:
-                print(f'Moving disk {rods[target][-1]} from {target} to {source}')
-                rods[source].append(rods[target].pop())
-            # display progress
-            print(rods)
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
         elif remainder == 0:
             print(f'Move {i + 1} allowed between {auxiliary} and {target}')
-
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
