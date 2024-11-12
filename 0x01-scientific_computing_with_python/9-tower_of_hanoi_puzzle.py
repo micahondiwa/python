91d6e62a894a3a4f59e33a993515c3e331eadd8e
micahondiwa <micah.oketch.ondiwa@gmail.ccom>
2024-11-12 16:23:04 +0300

    Make allowed move

--- 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
+++ 0x01-scientific_computing_with_python/9-tower_of_hanoi_puzzle.py
@@ -27,8 +27,9 @@ def move(n, source, auxiliary, target):
     for i in range(number_of_moves):
         remainder = (i + 1) % 3
         if remainder == 1:
-            print(f'Move {i + 1} allowed between {source} and {target}')
-            make_allowed_move(source,target)
+            if n % 2 != 0:
+                print(f'Move {i + 1} allowed between {source} and {target}')
+                make_allowed_move(source,target)
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
             make_allowed_move(source, auxiliary)
