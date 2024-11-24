96a89d96739a1ba05cc7dc07981bc8d3580f5d1d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-11-24 13:34:33 +0300

    Gitignore

--- 0x00-python/7-float.py
+++ 0x00-python/7-float.py
@@ -3,7 +3,6 @@
 * Convert it to float
 * Round it to 2 decimal places, and print it
 '''
-
 your_float = input("Enter your float: ")
 your_float = float(your_float)
 print("Round to 2 decimals: {:.2f}".format(your_float))
