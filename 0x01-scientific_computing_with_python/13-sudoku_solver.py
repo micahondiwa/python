5f21bc484af32b93f31a4500a7734f632e96250d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-12-04 11:03:11 +0300

    str method

--- 0x01-scientific_computing_with_python/13-sudoku_solver.py
+++ 0x01-scientific_computing_with_python/13-sudoku_solver.py
@@ -4,6 +4,8 @@ class Board:
     
     def __str__(self):
         board_str = ''
+        for row in self.board:
+            pass
     
     def find_empty_cell(self):
         for row, contents in enumerate(self.board):
