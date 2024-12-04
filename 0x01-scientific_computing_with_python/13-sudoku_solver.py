1ab57e827cb8724ab249a748a2b277fdf7995ec1
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-12-04 09:51:52 +0300

    Is valid

--- 0x01-scientific_computing_with_python/13-sudoku_solver.py
+++ 0x01-scientific_computing_with_python/13-sudoku_solver.py
@@ -32,6 +32,7 @@ class Board:
         valid_in_row = self.valid_in_row(row, num)
         valid_in_col = self.valid_in_col(col, num)
         valid_in_square = self.valid_in_square(row, col, num)
+        return all([valid_in_row, valid_in_col, valid_in_square])
 
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
