f1012774b5374f7d351ba27fe9295334f8fa92cb
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-12-04 10:57:41 +0300

    Solve sudoku

--- 0x01-scientific_computing_with_python/13-sudoku_solver.py
+++ 0x01-scientific_computing_with_python/13-sudoku_solver.py
@@ -51,6 +51,8 @@ def solve_sudoku(board):
     print(f'Puzzle to solve:\n{gameboard}')
     if gameboard.solver() == True:
         print(f'Solved puzzle:\n{gameboard}')
+    else:
+        print('The provided puzzle is unsolvable.')
 
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
