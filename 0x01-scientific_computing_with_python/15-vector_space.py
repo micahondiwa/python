fc26dd7dc158854401e39c57a0a54195674b8e4e
micahondiwa <micah.oketch.ondiwa@gmail.ccom>
2024-12-09 08:45:16 +0300

    String rep modified

--- 0x01-scientific_computing_with_python/15-vector_space.py
+++ 0x01-scientific_computing_with_python/15-vector_space.py
@@ -3,11 +3,14 @@ class R2Vector:
     def __init__(self, *, x, y):
         self.x = x
         self.y = y
-    def norm(self):
-        return sum(val**2 for val in self.__dict__.values())**0.5
-    def __str__(self):
-        return f'{self.x, self.y}'
 
+    def norm(self):
+        return sum(val**2 for val in vars(self).values())**0.5
+
+    def __str__(self):
+        return str(tuple(getattr(self, i) for i in vars(self)))
+    def __repr__(self):
+        pass
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
@@ -15,5 +18,5 @@ class R3Vector(R2Vector):
 
 v1 = R2Vector(x=2, y=3)
 v2 = R3Vector(x=2, y=2, z=3)
-print(v1.norm())
-print(v2.norm())
+print(f'v1 = {v1}')
+print(f'v2 = {v2}')
