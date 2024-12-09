ec0061185d28bd6cf5e4c168fb85e4b4e181a168
micahondiwa <micah.oketch.ondiwa@gmail.ccom>
2024-12-09 09:07:38 +0300

    String rep modified

--- 0x01-scientific_computing_with_python/15-vector_space.py
+++ 0x01-scientific_computing_with_python/15-vector_space.py
@@ -1,4 +1,3 @@
-
 class R2Vector:
     def __init__(self, *, x, y):
         self.x = x
@@ -9,14 +8,17 @@ class R2Vector:
 
     def __str__(self):
         return str(tuple(getattr(self, i) for i in vars(self)))
+
     def __repr__(self):
-        pass
+        arg_list = [f'{key}={val}' for key, val in vars(self).items()]
+        args = ', '.join(arg_list)
+        return f'{self.__class__.__name__}({args})'
+
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
-
 v1 = R2Vector(x=2, y=3)
 v2 = R3Vector(x=2, y=2, z=3)
-print(f'v1 = {v1}')
-print(f'v2 = {v2}')
+print(f'v1 = {v1}', f'\nrepr = {repr(v1)}')
+print(f'v2 = {v2}', f'\nrepr = {repr(v2)}')
