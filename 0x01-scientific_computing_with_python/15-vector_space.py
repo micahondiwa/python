c1498c1174cea0260cf9fa4460c89a2d0b991407
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-12-09 20:20:54 +0300

    Vector space

--- 0x01-scientific_computing_with_python/15-vector_space.py
+++ 0x01-scientific_computing_with_python/15-vector_space.py
@@ -6,9 +6,6 @@ class R2Vector:
     def norm(self):
         return sum(val**2 for val in vars(self).values())**0.5
 
-    def __str__(self):
-        return str(tuple(getattr(self, i) for i in vars(self)))
-
     def __repr__(self):
         arg_list = [f'{key}={val}' for key, val in vars(self).items()]
         args = ', '.join(arg_list)
@@ -18,6 +15,7 @@ class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
+
 v1 = R2Vector(x=2, y=3)
 v2 = R3Vector(x=2, y=2, z=3)
 print(f'v1 = {v1}', f'\nrepr = {repr(v1)}')
