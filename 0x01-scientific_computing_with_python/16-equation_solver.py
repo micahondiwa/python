3479e932eb856aa93c3f42695742f7bb2e25e7e5
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-12-18 19:55:05 +0300

    Highest degree must be greater than zero

--- 0x01-scientific_computing_with_python/16-equation_solver.py
+++ 0x01-scientific_computing_with_python/16-equation_solver.py
@@ -1,21 +1,21 @@
 from abc import ABC, abstractmethod
 
-
 class Equation(ABC):
     degree: int
-    
     def __init__(self, *args):
         if (self.degree + 1) != len(args):
             raise TypeError(
-                f"'{self.__class__.__name__}' object takes {self.degree + 1} positional arguments but {len(args)} were given"
+                f"'Equation' object takes {self.degree + 1} positional arguments but {len(args)} were given"
             )
         if any(not isinstance(arg, (int, float)) for arg in args):
             raise TypeError("Coefficients must be of type 'int' or 'float'")
+        if args[0] == 0:
+            raise ValueError('Highest degree coefficient must be different from zero')
     def __init_subclass__(cls):
         if not hasattr(cls, "degree"):
             raise AttributeError(
                 f"Cannot create '{cls.__name__}' class: missing required attribute 'degree'"
-            )
+            )        
     
     @abstractmethod
     def solve(self):
