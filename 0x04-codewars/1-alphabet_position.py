46279920d9557a29f8ce8f411eaf850cb59d19e7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-02-03 09:57:13 +0300

    Alphabet_position

---
+++ 0x04-codewars/1-alphabet_position.py
@@ -0,0 +1,10 @@
+"""
+Given a string, replace every letter with its
+position in the alphabet.If anything in the text isn't a letter,
+ignore and don't return it. 
+Example:
+Input = "The sunset sets at twelve o' clock."
+Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+"""
+
+def
