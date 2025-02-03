d7b68a2be465ffb9da78f157f33bbdb8ef38018e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-02-03 09:44:49 +0300

    Duplicates

---
+++ 0x04-codewars/duplicate_counts.py
@@ -0,0 +1,9 @@
+"""
+A function that will return the count of distinct case-insensitive
+alphabetic characters and numeric characters that occur more tha
+once in the input string. The inputs can be assumed to contain only
+alphabets (both upercase and lowercase) and numeric digits.
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"""
