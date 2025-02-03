a90dfdd7630c3bf1e688fa007fa47eab18287929
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-02-03 12:06:43 +0300

    Unique in order

--- 0x04-codewars/2-unique_in_order.py
+++ 0x04-codewars/2-unique_in_order.py
@@ -7,3 +7,10 @@ unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
 """
+def unique_in_order(sequence):
+    unique = []
+
+    for char in sequence:
+        if not unique or char =! unique[-1]:
+            unique.append(char)
+    return unique
