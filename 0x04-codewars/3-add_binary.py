76a8ccddb9043decea9c635b0edb417734ee259e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-02-03 12:47:39 +0300

    Add binary

---
+++ 0x04-codewars/3-add_binary.py
@@ -0,0 +1,8 @@
+""" 
+A function that adds two numbers and returns the sum in binary.
+The conversion can be done before or after the addition.
+The binary returned should be a string.
+Examples:
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+"""
