8fdaebc2183f2602f4477056f0ae00b1c94a33d9
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-02-03 10:44:39 +0300

    Alphabet_position

--- 0x04-codewars/1-alphabet_position.py
+++ 0x04-codewars/1-alphabet_position.py
@@ -1,10 +1,15 @@
 """
 Given a string, replace every letter with its
 position in the alphabet.If anything in the text isn't a letter,
-ignore and don't return it. 
+ignore and don't return it. "a" = 1, "b" = 2, etc.
 Example:
 Input = "The sunset sets at twelve o' clock."
 Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 """
 
-def
+def alphabet_position(text):
+    position = [str(ord(char) - 96) for char in text.lower() if char.isalpha()]
+    return ' '.join(position)
+
+Input = "The sunset sets at twelve o' clock."
+print(alphabet_position(text=input))
