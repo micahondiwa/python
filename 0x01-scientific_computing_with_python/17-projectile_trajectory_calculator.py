881587e1b211718abab491b3da2ce70fc5683f43
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2025-03-25 21:30:11 +0300

    Update 17-projectile_trajectory_calculator.py

--- 0x01-scientific_computing_with_python/17-projectile_trajectory_calculator.py
+++ 0x01-scientific_computing_with_python/17-projectile_trajectory_calculator.py
@@ -3,3 +3,9 @@ Implenting encapsulation by building a program that calculates and draws the tra
 given the angle, speed and height of the throw
 Encapsulation is a core OOP principle based on writing code that limits direct access to data.
 """
+import math
+
+GRAVITATIONAL_ACCELERATION = 9.81
+PROJECTILE = "∙"
+x_axis_tick = "T"
+y_axis_tick = "⊣"
