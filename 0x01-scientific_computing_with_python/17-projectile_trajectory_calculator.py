e689c342dfde7e57814c4c010870ce550200880d
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2025-03-25 21:47:15 +0300

    Update 17-projectile_trajectory_calculator.py

--- 0x01-scientific_computing_with_python/17-projectile_trajectory_calculator.py
+++ 0x01-scientific_computing_with_python/17-projectile_trajectory_calculator.py
@@ -9,3 +9,9 @@ GRAVITATIONAL_ACCELERATION = 9.81
 PROJECTILE = "∙"
 x_axis_tick = "T"
 y_axis_tick = "⊣"
+
+class Projectile:
+    def __init__(self, speed, height, angle):
+        self.__speed = speed
+        self.__height = height
+        self.__angle = math.radians(angle)
