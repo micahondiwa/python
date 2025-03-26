1ed98b909fa2360c528b1d601b8a8ae77045c5e1
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2025-03-26 20:55:43 +0300

    Update 17-projectile_trajectory_calculator.py

--- 0x01-scientific_computing_with_python/17-projectile_trajectory_calculator.py
+++ 0x01-scientific_computing_with_python/17-projectile_trajectory_calculator.py
@@ -1,6 +1,6 @@
 """
 Implenting encapsulation by building a program that calculates and draws the trajectory of a projectile 
-given the angle, speed and height of the throw
+given the angle, speed and height of the throw.
 Encapsulation is a core OOP principle based on writing code that limits direct access to data.
 """
 import math
